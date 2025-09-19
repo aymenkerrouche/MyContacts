f098d594f5496864ff9609f85f5c419d3ec1f5f8
Aymen <aymene.kerrouche@univ-constantine2.dz>
2025-09-19 14:55:41 +0200

    test cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,26 +8,33 @@ const swaggerDocs = require("./docs/swagger");
 
 const app = express();
 
+// ------------------------------------------------ 
+
 // Middlewares
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
-    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: true
   })
 );
 app.use(morgan("dev"));
 
+
+
+
+// ------------------------------------------------ 
 // Routes
 app.use("/auth", authRoutes);
 app.use("/contacts", contactRoutes);
 
+
+
+// ------------------------------------------------ 
 // Swagger
 swaggerDocs(app);
 
 app.get("/", (req, res) => {
-  res.json({ message: "API MyContacts is running 🚀" });
+  res.json({ message: "API MyContacts is running" });
 });
 
 module.exports = app;
